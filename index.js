783a72adf8ca7e1a7f532dbc5602f36fe0319cdd
malyesh <74512928+malyesh@users.noreply.github.com>
2021-12-10 13:36:27 -0500

    more changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -10,7 +11,6 @@ app.get('/Weather-App/index.html', (request, response) => {
   response.sendFile(__dirname + '/Weather-App/index.html');
 });
 
-// app.listen(port, () => {
-//   console.log(`Webpage listening at http://localhost:${port}`);
-// });
-app.listen('https://malyesh.github.io/');
+app.listen(port, () => {
+  console.log(`Webpage listening at http://localhost:${port}`);
+});
